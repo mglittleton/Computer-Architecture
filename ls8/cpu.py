49d4d2f6495724b9dacc844033cd7b53cc6095cc
Michael Littleton <michaelglittleton@gmail.com>
2019-08-22 00:58:04 -0400

    Load completed

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,39 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.reg = [0] * 8
+        self.ram = [0] * 25
+        self.pc = 0
+        self.ir = None
+        self.fl = 0b00000000
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
+
+        # convert file into list of binary codes and enter into ram below
+        file_lines = open(file, 'r')
+        program = file_lines.readlines()
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
         for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+            letter1 = instruction[0]
+            if letter1 == '0' or letter1 == '1':
+                self.ram[address] = int(instruction[:8], 2)
+                address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +46,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ir,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +60,14 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        
+        self.trace()
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
+
+    def HLT(self):
+        exit()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,14 @@
 import sys
 from cpu import *
 
+args = sys.argv
+if len(args) < 2:
+  print("missing argument - format should be: python ls8.py <path to program file>")
+  exit()
+else:
+  file = sys.argv[1]
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(file)
 cpu.run()
